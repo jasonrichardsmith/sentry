021386d3e6f6797e7e54bf0ace82bdff86bc20cb
Jason Smith <jasonrichardsmith@gmail.com>
2018-08-14 20:41:29 +0200

    more tests

--- limits/limits_test.go
+++ limits/limits_test.go
@@ -3,6 +3,9 @@ package limits
 import (
 	"io/ioutil"
 	"log"
+	"testing"
+
+	"k8s.io/apimachinery/pkg/api/resource"
 )
 
 var (
@@ -15,4 +18,29 @@ func init() {
 	if err != nil {
 		log.Fatal(err)
 	}
+}
+
+func TestBetweenCPU(t *testing.T) {
+	lowqty, err := resource.ParseQuantity("1G")
+	if err != nil {
+		log.Fatal(err)
+	}
+	var highqty resource.Quantity
+	highqty, err = resource.ParseQuantity("2G")
+	if err != nil {
+		log.Fatal(err)
+	}
+	var test resource.Quantity
+	test, err = resource.ParseQuantity("1.5G")
+	if err != nil {
+		log.Fatal(err)
+	}
+	ls := LimitSentry{
+		CPUMax: highqty,
+		CPUMin: lowqty,
+	}
+	if !ls.BetweenCPU(test) {
+		t.Error("expected qty for CPU to be in between")
+	}
+
 }
