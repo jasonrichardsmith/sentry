a2222d57cf455f2b3b945dd1f005fd5ddcf8a005
Jason Smith <jasonrichardsmith@gmail.com>
2018-09-01 11:09:43 +0200

    added better docs and an example module

---
+++ example/example_test.go
@@ -0,0 +1,30 @@
+package example
+
+import (
+	"testing"
+
+	"k8s.io/api/admission/v1beta1"
+	"k8s.io/apimachinery/pkg/runtime"
+)
+
+func TestType(t *testing.T) {
+	ts := ExampleSentry{}
+	if ts.Type() != "Pod" {
+		t.Fatal("Failed type test")
+	}
+}
+
+func TestAdmit(t *testing.T) {
+	s := ExampleSentry{}
+	ar := v1beta1.AdmissionReview{
+		Request: &v1beta1.AdmissionRequest{
+			Object: runtime.RawExtension{
+				Raw: make([]byte, 0),
+			},
+		},
+	}
+	resp := s.Admit(ar)
+	if !resp.Allowed {
+		t.Fatal("expected passing review")
+	}
+}
