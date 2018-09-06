aa2215405041f5fb6b23650a8634a21db866c03a
Jason Smith <jasonrichardsmith@gmail.com>
2018-09-06 17:35:25 +0200

    Test coverage (#26)
    * improved example coverage
    
    * more tests and fixed bug in config enabling
    
    * example coverage
    
    * healthz coverage
    
    * more tags tests
    
    * better source tests
    
    * added new limit tests
    
    * added mux test

--- limits/config_test.go
+++ limits/config_test.go
@@ -1,1 +1,44 @@
 package limits
+
+import (
+	"reflect"
+	"testing"
+
+	"k8s.io/apimachinery/pkg/api/resource"
+)
+
+func TestName(t *testing.T) {
+	c := Config{}
+	if c.Name() != "limits" {
+		t.Fatal("Failed name test")
+	}
+}
+
+func TestQtyHook(t *testing.T) {
+	var i int
+	test := "1G"
+	vtype := reflect.TypeOf(i)
+	v, err := QtyHookFunc(vtype, vtype, test)
+	if err != nil {
+		t.Fatal(err)
+	}
+	if v.(string) != test {
+		t.Fatal("Expecting skip on not being string")
+	}
+	stype := reflect.TypeOf(test)
+	v, err = QtyHookFunc(stype, vtype, test)
+	if err != nil {
+		t.Fatal(err)
+	}
+	if v.(string) != test {
+		t.Fatal("Expecting skip on not being qty")
+	}
+	vtype = reflect.TypeOf(resource.Quantity{})
+	v, err = QtyHookFunc(stype, vtype, test)
+	if err != nil {
+		t.Fatal(err)
+	}
+	if reflect.TypeOf(resource.Quantity{}) != reflect.TypeOf(v) {
+		t.Fatal("Expecting return of qty")
+	}
+}
--- mux/mux_test.go
+++ mux/mux_test.go
@@ -3,6 +3,9 @@ package mux
 import (
 	"testing"
 
+	"github.com/jasonrichardsmith/sentry/config"
+	"github.com/jasonrichardsmith/sentry/sentry"
+
 	"k8s.io/api/admission/v1beta1"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 )
@@ -27,6 +30,18 @@ func TestType(t *testing.T) {
 		t.Fatal("Failed type test")
 	}
 }
+
+type FakeModuleConfig struct {
+}
+
+func (f FakeModuleConfig) LoadSentry() sentry.Sentry { return FakeSentry{} }
+func (f FakeModuleConfig) Name() string              { return "one" }
+
+type FakeModuleConfig2 struct {
+}
+
+func (f FakeModuleConfig2) LoadSentry() sentry.Sentry { return FakeSentry{} }
+func (f FakeModuleConfig2) Name() string              { return "two" }
 
 type FakeSentry struct {
 	admit bool
@@ -85,6 +100,21 @@ func TestAdmit(t *testing.T) {
 	resp = mux.Admit(ar)
 	if resp.Allowed != false {
 		t.Fatal("Return true expected false")
+	}
+
+}
+
+func TestNew(t *testing.T) {
+	c := config.New()
+	sm := New(c)
+	if sm.Sentries == nil || len(sm.Sentries) > 0 {
+		t.Fatal("expected non nil slice of 0 length")
+	}
+	c.Register(FakeModuleConfig{})
+	c.Register(FakeModuleConfig2{})
+	sm = New(c)
+	if len(sm.Sentries) != 2 {
+		t.Fatal("expected 2 modules")
 	}
 
 }
